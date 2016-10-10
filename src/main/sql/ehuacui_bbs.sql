73751ca2de0778b079d613685d7c73ed45a855b5
jianwei.zhou <zhoujianwei1987@gmail.com>
2016-10-10 10:27:48 +0800

    数据库表重命名

--- src/main/sql/ehuacui_bbs.sql
+++ src/main/sql/ehuacui_bbs.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : 127.0.0.1
 Source Server Version : 50626
 Source Host           : 127.0.0.1:3306
-Source Database       : ehuacui_bbs
+Source Database       : bbs
 
 Target Server Type    : MYSQL
 Target Server Version : 50626
@@ -16,10 +16,10 @@ Date: 2016-08-14 21:52:39
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for ehuacui_collect
+-- Table structure for tb_collect
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_collect`;
-CREATE TABLE `ehuacui_collect` (
+DROP TABLE IF EXISTS `tb_collect`;
+CREATE TABLE `tb_collect` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tid` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
@@ -28,14 +28,14 @@ CREATE TABLE `ehuacui_collect` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of ehuacui_collect
+-- Records of tb_collect
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for ehuacui_notification
+-- Table structure for tb_notification
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_notification`;
-CREATE TABLE `ehuacui_notification` (
+DROP TABLE IF EXISTS `tb_notification`;
+CREATE TABLE `tb_notification` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `read` tinyint(1) NOT NULL COMMENT '是否已读：0默认 1已读',
   `author` varchar(50) NOT NULL DEFAULT '' COMMENT '发起通知用户昵称',
@@ -48,14 +48,14 @@ CREATE TABLE `ehuacui_notification` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_notification
+-- Records of tb_notification
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for ehuacui_permission
+-- Table structure for tb_permission
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_permission`;
-CREATE TABLE `ehuacui_permission` (
+DROP TABLE IF EXISTS `tb_permission`;
+CREATE TABLE `tb_permission` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL DEFAULT '' COMMENT '权限名称',
   `url` varchar(255) DEFAULT NULL COMMENT '授权路径',
@@ -65,47 +65,47 @@ CREATE TABLE `ehuacui_permission` (
 ) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_permission
+-- Records of tb_permission
 -- ----------------------------
-INSERT INTO `ehuacui_permission` VALUES ('55', 'section', '', '板块节点', '0');
-INSERT INTO `ehuacui_permission` VALUES ('56', 'system', '', '系统设置', '0');
-INSERT INTO `ehuacui_permission` VALUES ('57', 'topic', '', '话题节点', '0');
-INSERT INTO `ehuacui_permission` VALUES ('58', 'reply', '', '回复节点', '0');
-INSERT INTO `ehuacui_permission` VALUES ('59', 'system:users', '/manage/users', '用户列表', '56');
-INSERT INTO `ehuacui_permission` VALUES ('60', 'system:roles', '/manage/roles', '角色列表', '56');
-INSERT INTO `ehuacui_permission` VALUES ('61', 'system:permissions', '/manage/permissions', '权限列表', '56');
-INSERT INTO `ehuacui_permission` VALUES ('62', 'system:user:role', '/manage/userrole', '用户角色关联', '56');
-INSERT INTO `ehuacui_permission` VALUES ('63', 'system:role:permission', '/manage/rolepermission', '角色权限关联', '56');
-INSERT INTO `ehuacui_permission` VALUES ('64', 'system:add:permission', '/manage/addpermission', '添加权限', '56');
-INSERT INTO `ehuacui_permission` VALUES ('65', 'system:edit:permission', '/manage/editpermission', '编辑权限', '56');
-INSERT INTO `ehuacui_permission` VALUES ('66', 'system:add:role', '/manage/addrole', '添加角色', '56');
-INSERT INTO `ehuacui_permission` VALUES ('67', 'system:add:role', '/manage/addrole', '添加角色', '56');
-INSERT INTO `ehuacui_permission` VALUES ('68', 'system:delete:user', '/manage/deleteuser', '删除用户', '56');
-INSERT INTO `ehuacui_permission` VALUES ('69', 'system:delete:role', '/manage/deleterole', '删除角色', '56');
-INSERT INTO `ehuacui_permission` VALUES ('70', 'system:delete:permission', '/manage/deletepermission', '删除权限', '56');
-INSERT INTO `ehuacui_permission` VALUES ('71', 'topic:delete', '/topic/delete', '删除话题', '57');
-INSERT INTO `ehuacui_permission` VALUES ('73', 'reply:delete', '/reply/delete', '删除回复', '58');
-INSERT INTO `ehuacui_permission` VALUES ('74', 'reply:edit', '/reply/edit', '编辑回复', '58');
-INSERT INTO `ehuacui_permission` VALUES ('75', 'topic:edit', '/topic/edit', '话题编辑', '57');
-INSERT INTO `ehuacui_permission` VALUES ('76', 'topic:append:edit', '/topic/appendedit', '追加编辑', '57');
-INSERT INTO `ehuacui_permission` VALUES ('77', 'topic:top', '/topic/top', '话题置顶', '57');
-INSERT INTO `ehuacui_permission` VALUES ('78', 'topic:good', '/topic/good', '话题加精', '57');
-INSERT INTO `ehuacui_permission` VALUES ('79', 'system:clear:cache', '/clear', '删除所有缓存', '56');
-INSERT INTO `ehuacui_permission` VALUES ('80', 'system:user:block', '/manage/userblock', '禁用用户', '56');
-INSERT INTO `ehuacui_permission` VALUES ('81', 'section:list', '/section/list', '板块列表', '55');
-INSERT INTO `ehuacui_permission` VALUES ('82', 'section:change:show:status', '/section/changeshowstatus', '改变板块显示状态', '55');
-INSERT INTO `ehuacui_permission` VALUES ('83', 'section:delete', '/section/delete', '删除板块', '55');
-INSERT INTO `ehuacui_permission` VALUES ('84', 'section:add', '/section/add', '添加板块', '55');
-INSERT INTO `ehuacui_permission` VALUES ('85', 'section:edit', '/section/edit', '编辑板块', '55');
-INSERT INTO `ehuacui_permission` VALUES ('86', 'reply:list', '/reply/list', '回复列表', '58');
-INSERT INTO `ehuacui_permission` VALUES ('87', 'system:solr', '/solr', '索引所有话题(慎用)', '56');
-INSERT INTO `ehuacui_permission` VALUES ('88', 'system:delete:all:index', '/deleteallindex', '删除所有索引', '56');
+INSERT INTO `tb_permission` VALUES ('55', 'section', '', '板块节点', '0');
+INSERT INTO `tb_permission` VALUES ('56', 'system', '', '系统设置', '0');
+INSERT INTO `tb_permission` VALUES ('57', 'topic', '', '话题节点', '0');
+INSERT INTO `tb_permission` VALUES ('58', 'reply', '', '回复节点', '0');
+INSERT INTO `tb_permission` VALUES ('59', 'system:users', '/manage/users', '用户列表', '56');
+INSERT INTO `tb_permission` VALUES ('60', 'system:roles', '/manage/roles', '角色列表', '56');
+INSERT INTO `tb_permission` VALUES ('61', 'system:permissions', '/manage/permissions', '权限列表', '56');
+INSERT INTO `tb_permission` VALUES ('62', 'system:user:role', '/manage/userrole', '用户角色关联', '56');
+INSERT INTO `tb_permission` VALUES ('63', 'system:role:permission', '/manage/rolepermission', '角色权限关联', '56');
+INSERT INTO `tb_permission` VALUES ('64', 'system:add:permission', '/manage/addpermission', '添加权限', '56');
+INSERT INTO `tb_permission` VALUES ('65', 'system:edit:permission', '/manage/editpermission', '编辑权限', '56');
+INSERT INTO `tb_permission` VALUES ('66', 'system:add:role', '/manage/addrole', '添加角色', '56');
+INSERT INTO `tb_permission` VALUES ('67', 'system:add:role', '/manage/addrole', '添加角色', '56');
+INSERT INTO `tb_permission` VALUES ('68', 'system:delete:user', '/manage/deleteuser', '删除用户', '56');
+INSERT INTO `tb_permission` VALUES ('69', 'system:delete:role', '/manage/deleterole', '删除角色', '56');
+INSERT INTO `tb_permission` VALUES ('70', 'system:delete:permission', '/manage/deletepermission', '删除权限', '56');
+INSERT INTO `tb_permission` VALUES ('71', 'topic:delete', '/topic/delete', '删除话题', '57');
+INSERT INTO `tb_permission` VALUES ('73', 'reply:delete', '/reply/delete', '删除回复', '58');
+INSERT INTO `tb_permission` VALUES ('74', 'reply:edit', '/reply/edit', '编辑回复', '58');
+INSERT INTO `tb_permission` VALUES ('75', 'topic:edit', '/topic/edit', '话题编辑', '57');
+INSERT INTO `tb_permission` VALUES ('76', 'topic:append:edit', '/topic/appendedit', '追加编辑', '57');
+INSERT INTO `tb_permission` VALUES ('77', 'topic:top', '/topic/top', '话题置顶', '57');
+INSERT INTO `tb_permission` VALUES ('78', 'topic:good', '/topic/good', '话题加精', '57');
+INSERT INTO `tb_permission` VALUES ('79', 'system:clear:cache', '/clear', '删除所有缓存', '56');
+INSERT INTO `tb_permission` VALUES ('80', 'system:user:block', '/manage/userblock', '禁用用户', '56');
+INSERT INTO `tb_permission` VALUES ('81', 'section:list', '/section/list', '板块列表', '55');
+INSERT INTO `tb_permission` VALUES ('82', 'section:change:show:status', '/section/changeshowstatus', '改变板块显示状态', '55');
+INSERT INTO `tb_permission` VALUES ('83', 'section:delete', '/section/delete', '删除板块', '55');
+INSERT INTO `tb_permission` VALUES ('84', 'section:add', '/section/add', '添加板块', '55');
+INSERT INTO `tb_permission` VALUES ('85', 'section:edit', '/section/edit', '编辑板块', '55');
+INSERT INTO `tb_permission` VALUES ('86', 'reply:list', '/reply/list', '回复列表', '58');
+INSERT INTO `tb_permission` VALUES ('87', 'system:solr', '/solr', '索引所有话题(慎用)', '56');
+INSERT INTO `tb_permission` VALUES ('88', 'system:delete:all:index', '/deleteallindex', '删除所有索引', '56');
 
 -- ----------------------------
--- Table structure for ehuacui_reply
+-- Table structure for tb_reply
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_reply`;
-CREATE TABLE `ehuacui_reply` (
+DROP TABLE IF EXISTS `tb_reply`;
+CREATE TABLE `tb_reply` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tid` int(11) NOT NULL COMMENT '话题id',
   `content` longtext NOT NULL COMMENT '回复内容',
@@ -116,14 +116,14 @@ CREATE TABLE `ehuacui_reply` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_reply
+-- Records of tb_reply
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for ehuacui_role
+-- Table structure for tb_role
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_role`;
-CREATE TABLE `ehuacui_role` (
+DROP TABLE IF EXISTS `tb_role`;
+CREATE TABLE `tb_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL DEFAULT '',
   `description` varchar(255) DEFAULT NULL,
@@ -131,17 +131,17 @@ CREATE TABLE `ehuacui_role` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_role
+-- Records of tb_role
 -- ----------------------------
-INSERT INTO `ehuacui_role` VALUES ('1', 'admin', '超级管理员');
-INSERT INTO `ehuacui_role` VALUES ('2', 'banzhu', '版主');
-INSERT INTO `ehuacui_role` VALUES ('3', 'user', '普通用户');
+INSERT INTO `tb_role` VALUES ('1', 'admin', '超级管理员');
+INSERT INTO `tb_role` VALUES ('2', 'banzhu', '版主');
+INSERT INTO `tb_role` VALUES ('3', 'user', '普通用户');
 
 -- ----------------------------
--- Table structure for ehuacui_role_permission
+-- Table structure for tb_role_permission
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_role_permission`;
-CREATE TABLE `ehuacui_role_permission` (
+DROP TABLE IF EXISTS `tb_role_permission`;
+CREATE TABLE `tb_role_permission` (
   `rid` int(11) NOT NULL,
   `pid` int(11) NOT NULL,
   KEY `fk_role_permission` (`rid`),
@@ -149,50 +149,50 @@ CREATE TABLE `ehuacui_role_permission` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_role_permission
+-- Records of tb_role_permission
 -- ----------------------------
-INSERT INTO `ehuacui_role_permission` VALUES ('2', '71');
-INSERT INTO `ehuacui_role_permission` VALUES ('2', '75');
-INSERT INTO `ehuacui_role_permission` VALUES ('2', '76');
-INSERT INTO `ehuacui_role_permission` VALUES ('2', '77');
-INSERT INTO `ehuacui_role_permission` VALUES ('2', '78');
-INSERT INTO `ehuacui_role_permission` VALUES ('2', '73');
-INSERT INTO `ehuacui_role_permission` VALUES ('2', '74');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '59');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '60');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '61');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '62');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '63');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '64');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '65');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '66');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '67');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '68');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '69');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '70');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '79');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '87');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '88');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '89');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '71');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '75');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '76');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '77');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '78');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '73');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '74');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '86');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '81');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '82');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '83');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '84');
-INSERT INTO `ehuacui_role_permission` VALUES ('1', '85');
+INSERT INTO `tb_role_permission` VALUES ('2', '71');
+INSERT INTO `tb_role_permission` VALUES ('2', '75');
+INSERT INTO `tb_role_permission` VALUES ('2', '76');
+INSERT INTO `tb_role_permission` VALUES ('2', '77');
+INSERT INTO `tb_role_permission` VALUES ('2', '78');
+INSERT INTO `tb_role_permission` VALUES ('2', '73');
+INSERT INTO `tb_role_permission` VALUES ('2', '74');
+INSERT INTO `tb_role_permission` VALUES ('1', '59');
+INSERT INTO `tb_role_permission` VALUES ('1', '60');
+INSERT INTO `tb_role_permission` VALUES ('1', '61');
+INSERT INTO `tb_role_permission` VALUES ('1', '62');
+INSERT INTO `tb_role_permission` VALUES ('1', '63');
+INSERT INTO `tb_role_permission` VALUES ('1', '64');
+INSERT INTO `tb_role_permission` VALUES ('1', '65');
+INSERT INTO `tb_role_permission` VALUES ('1', '66');
+INSERT INTO `tb_role_permission` VALUES ('1', '67');
+INSERT INTO `tb_role_permission` VALUES ('1', '68');
+INSERT INTO `tb_role_permission` VALUES ('1', '69');
+INSERT INTO `tb_role_permission` VALUES ('1', '70');
+INSERT INTO `tb_role_permission` VALUES ('1', '79');
+INSERT INTO `tb_role_permission` VALUES ('1', '87');
+INSERT INTO `tb_role_permission` VALUES ('1', '88');
+INSERT INTO `tb_role_permission` VALUES ('1', '89');
+INSERT INTO `tb_role_permission` VALUES ('1', '71');
+INSERT INTO `tb_role_permission` VALUES ('1', '75');
+INSERT INTO `tb_role_permission` VALUES ('1', '76');
+INSERT INTO `tb_role_permission` VALUES ('1', '77');
+INSERT INTO `tb_role_permission` VALUES ('1', '78');
+INSERT INTO `tb_role_permission` VALUES ('1', '73');
+INSERT INTO `tb_role_permission` VALUES ('1', '74');
+INSERT INTO `tb_role_permission` VALUES ('1', '86');
+INSERT INTO `tb_role_permission` VALUES ('1', '81');
+INSERT INTO `tb_role_permission` VALUES ('1', '82');
+INSERT INTO `tb_role_permission` VALUES ('1', '83');
+INSERT INTO `tb_role_permission` VALUES ('1', '84');
+INSERT INTO `tb_role_permission` VALUES ('1', '85');
 
 -- ----------------------------
--- Table structure for ehuacui_section
+-- Table structure for tb_section
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_section`;
-CREATE TABLE `ehuacui_section` (
+DROP TABLE IF EXISTS `tb_section`;
+CREATE TABLE `tb_section` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL DEFAULT '' COMMENT '板块名称',
   `tab` varchar(45) NOT NULL DEFAULT '' COMMENT '板块标签',
@@ -205,18 +205,18 @@ CREATE TABLE `ehuacui_section` (
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_section
+-- Records of tb_section
 -- ----------------------------
-INSERT INTO `ehuacui_section` VALUES ('1', '问答', 'ask', '1', '4', '0', '0');
-INSERT INTO `ehuacui_section` VALUES ('2', '博客', 'blog', '1', '5', '0', '0');
-INSERT INTO `ehuacui_section` VALUES ('3', '资讯', 'news', '1', '2', '0', '0');
-INSERT INTO `ehuacui_section` VALUES ('4', '分享', 'share', '1', '3', '1', '0');
+INSERT INTO `tb_section` VALUES ('1', '问答', 'ask', '1', '4', '0', '0');
+INSERT INTO `tb_section` VALUES ('2', '博客', 'blog', '1', '5', '0', '0');
+INSERT INTO `tb_section` VALUES ('3', '资讯', 'news', '1', '2', '0', '0');
+INSERT INTO `tb_section` VALUES ('4', '分享', 'share', '1', '3', '1', '0');
 
 -- ----------------------------
--- Table structure for ehuacui_topic
+-- Table structure for tb_topic
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_topic`;
-CREATE TABLE `ehuacui_topic` (
+DROP TABLE IF EXISTS `tb_topic`;
+CREATE TABLE `tb_topic` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tab` varchar(50) NOT NULL COMMENT '版块标识',
   `title` varchar(128) NOT NULL COMMENT '话题标题',
@@ -238,14 +238,14 @@ CREATE TABLE `ehuacui_topic` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_topic
+-- Records of tb_topic
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for ehuacui_topic_append
+-- Table structure for tb_topic_append
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_topic_append`;
-CREATE TABLE `ehuacui_topic_append` (
+DROP TABLE IF EXISTS `tb_topic_append`;
+CREATE TABLE `tb_topic_append` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tid` int(11) NOT NULL,
   `content` longtext NOT NULL,
@@ -255,14 +255,14 @@ CREATE TABLE `ehuacui_topic_append` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of ehuacui_topic_append
+-- Records of tb_topic_append
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for ehuacui_user
+-- Table structure for tb_user
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_user`;
-CREATE TABLE `ehuacui_user` (
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nickname` varchar(50) NOT NULL DEFAULT '' COMMENT '昵称',
   `score` int(11) NOT NULL COMMENT '积分',
@@ -283,14 +283,14 @@ CREATE TABLE `ehuacui_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_user
+-- Records of tb_user
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for ehuacui_user_role
+-- Table structure for tb_user_role
 -- ----------------------------
-DROP TABLE IF EXISTS `ehuacui_user_role`;
-CREATE TABLE `ehuacui_user_role` (
+DROP TABLE IF EXISTS `tb_user_role`;
+CREATE TABLE `tb_user_role` (
   `uid` int(11) NOT NULL,
   `rid` int(11) NOT NULL,
   KEY `fk_user_role` (`uid`),
@@ -298,6 +298,6 @@ CREATE TABLE `ehuacui_user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
--- Records of ehuacui_user_role
+-- Records of tb_user_role
 -- ----------------------------
-INSERT INTO `ehuacui_user_role` VALUES ('1', '1');
+INSERT INTO `tb_user_role` VALUES ('1', '1');
